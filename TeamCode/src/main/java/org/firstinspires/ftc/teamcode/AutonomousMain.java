0487c5142cc740ac8b6efe9a534dbd66fb875c81
damarlavarun2004 <damarlav@uscstudents.org>
2020-01-19 08:23:36 -0500

    Combined the foundation and block autonomous. However, this theory needs to be tested ASAP!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMain.java
@@ -24,7 +24,7 @@ class AutonomousMain {
     // Declare OpMode Members
 
     private static final double DRIVE_SPEED = 0.7;
-    private static final double TURN_SPEED = 0.5;
+    private static final double TURN_SPEED = 0.6;
     private static final double SERVO_STEP = 0.01;
 
     Robot robot;
@@ -106,8 +106,9 @@ class AutonomousMain {
                 AngleUnit.DEGREES
         ).firstAngle;
 
-        // shared();
-        /*
+
+        /* Block Autonomous (Working)
+        shared();
         robot.gyroDrive(0, DRIVE_SPEED, 20, 0, true);
         sleep(750);
         robot.getMovement().openGrabber(false);
@@ -123,6 +124,7 @@ class AutonomousMain {
         robot.gyroDrive(0,DRIVE_SPEED, -15, 0, true);
         */
 
+        /* Foundation Autonomous (In Progress)
         robot.gyroDrive(0, DRIVE_SPEED, 20, 0, true);
         robot.gyroTurn(0, TURN_SPEED, -90+offset);
         robot.gyroDrive(0, DRIVE_SPEED, 61, 0, true);
@@ -139,19 +141,29 @@ class AutonomousMain {
         robot.gyroTurn(0, TURN_SPEED, 90+offset);
         robot.gyroDrive(0, DRIVE_SPEED, 22, 0,true);
         shared();
+        */
 
-        /*
-        robot.getMovement().openGrabber(false);
-        sleep(3000);
-        robot.getMovement().openGrabber(true);
-        sleep(1500);
-        robot.gyroDrive(0, DRIVE_SPEED, -10, 0, true);
-        robot.gyroTurn(0, TURN_SPEED, -90 + offset);
-        robot.gyroDrive(0, DRIVE_SPEED, 58, 0, true);
-        robot.getMovement().openGrabber(false);
-        sleep(1500);
-        robot.gyroDrive(0, DRIVE_SPEED, -20, 0, true);
-         */
+        // Blocks + Autonomous (In Progress)
+        shared(); // Preparation for block grabbing
+        robot.gyroDrive(0, DRIVE_SPEED, 18, 0, true); // Robot approaches the block
+        sleep(500); // Wait time to prepare the grabber for grabbing the block
+        robot.getMovement().openGrabber(false); // Robot grabs the block
+        sleep(500); // Robot process the grab to avoid unintentional errors
+        robot.gyroTurn(0, TURN_SPEED, 90+offset); // Robot turns toward the alliance bridge
+        robot.gyroDrive(0, DRIVE_SPEED, 61, 0, true); // Robot drives under the alliance bridge with the block
+        sleep(500); // Wait time to prepare robot to release block
+        robot.getMovement().openGrabber(true); // Robot releases the block
+        robot.gyroTurn(0, TURN_SPEED, 0+offset); // Robot turns toward the foundation
+        robot.gyroDrive(0, DRIVE_SPEED, 2, 0, true); // Robot approaches the foundation
+        robot.getMovement().grabFoundation(true); // Robot grabs the foundation
+        sleep(500); // Wait time for robot to process the foundation grab
+        robot.gyroDrive(0, DRIVE_SPEED, -15, 0, true); // Robot moves the foundation back
+        robot.gyroTurn(0, TURN_SPEED, -26+offset); // Robot turns to angle foundation for placement in the building site
+        sleep(500); // Wait time to process the angling
+        robot.gyroDrive(0, DRIVE_SPEED,-18, 0, true); // Robot moves the foundation close to the building zone
+        robot.getMovement().grabFoundation(false); // Robot releases foundation
+        robot.gyroTurn(0, TURN_SPEED, 90+offset); // Robot turns to knock foundation into building site
+        robot.gyroDrive(0, DRIVE_SPEED, 22, 0,true); // Robot drives under alliance bridge and parks
     }
 
     private void shared() {
