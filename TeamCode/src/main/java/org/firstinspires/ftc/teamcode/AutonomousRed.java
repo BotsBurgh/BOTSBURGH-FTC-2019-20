69ac6e3d4770ff2e8977bd3a5c71a8a1ae6db4c4
Sambhav Saggi <smsaggi@gmail.com>
2020-01-19 09:38:45 -0500

    Disable old opmodes
    Disable unused old opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRed.java
@@ -17,10 +17,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Autonomous Red", group="Autonomous")
+@Disabled
 public class AutonomousRed extends LinearOpMode {
     // Declare OpMode Members
     private ElapsedTime runtime = new ElapsedTime();
